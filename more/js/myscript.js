c26e063592d523df69b8f099a9bb2f0b5c9ffbd2
Yurko Dmitriev <4au.4au.4au@gmail.com>
2017-03-12 15:50:21 +0200

    tgt

--- more/js/myscript.js
+++ more/js/myscript.js
@@ -1,3 +1,14 @@
+"use strict";
+
+var menuToggle = function(){
+	$('.open_menu').click(function(){
+		$('.mobile_menu').addClass('act');
+	});
+	$('.close_menu').click(function(){
+		$('.mobile_menu').removeClass('act');
+	});
+}
+
 $(window).load(function(){
-	
+	menuToggle();
 });
