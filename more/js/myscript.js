129749ae9ea047016085e26a8ef7cb3c03a9037c
Yurko Dmitriev <4au.4au.4au@gmail.com>
2017-03-15 20:59:28 +0200

    kk

--- more/js/myscript.js
+++ more/js/myscript.js
@@ -9,6 +9,79 @@ var menuToggle = function(){
 	});
 }
 
+var hidePlaceholder = function(){
+	$('input,textarea').focus(function(){
+	   $(this).data('placeholder',$(this).attr('placeholder'))
+	          .attr('placeholder','');
+	}).blur(function(){
+	   $(this).attr('placeholder',$(this).data('placeholder'));
+	});
+}
+
+var formValidate = function(){
+	$('#form_send').click(function(){
+
+        var name = $('#form #name').val();
+        var email = $('#form #email').val();
+        var message = $('#form #message').val();
+
+        function isValidEmailAddress(email) {
+		    var pattern = /^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
+		    return pattern.test(email);
+		};
+		function isValidPhone(phone) {
+		    var pattern = /[0-9\-\(\)\s]/;
+		    return pattern.test(phone);
+		};
+
+		if (name.length < 1) {
+			$('#form #name').addClass('error');
+		} else {
+			$('#form #name').removeClass('error');
+			var validName = $('#form #name').val();
+		}
+
+
+		if (email.length < 1 || !isValidEmailAddress( email )) {
+			$('#form #email').addClass('error');
+		} else {
+			$('#form #email').removeClass('error');
+			var validEmail = $('#form #email').val();
+		}
+
+		if (message.length < 1 ) {
+			$('#form #message').addClass('error');
+		} else {
+			$('#form #message').removeClass('error');
+			var validmessage = $('#form #message').val();
+		}
+
+
+        if (!validName || !validEmail || !validAddress ) {
+        	return false;
+        }
+
+
+        $.ajax({  
+		    type: 'POST',  
+		    url: 'sendmail.php', 
+		    data: { 
+		    	name : name,
+		    	email : email,
+		    	address : address
+		    },
+		    success: function(data) {
+		        
+		    },
+		    error: function (data) {
+                
+            }
+		});
+    });
+}
+
 $(window).load(function(){
 	menuToggle();
+	hidePlaceholder();
+	formValidate();
 });
---
+++ more/sendmail.php
@@ -0,0 +1,45 @@
+<?php
+$name = isset($_POST['name']) ? $_POST['name'] : '';
+$email = isset($_POST['email']) ? $_POST['email'] : '';
+$address = isset($_POST['address']) ? $_POST['address'] : '';
+
+if(empty($name) || empty($email) || empty($phone) || empty($address) ){
+    header('HTTP/1.1 500 Internal Server Booboo');
+    header('Content-Type: application/json; charset=UTF-8');
+    die(json_encode(array('message' => 'Some field is empty', 'code' => 500)));
+}
+
+$headers = 'From: ' . $name . '<' . $email . '>' . "\r\n" .
+    'Content-type: text/html;' .
+    'Reply-To: ' . $email . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+
+    $message = '
+        <table border="1">
+            <tr>
+                <td>Name:</td>
+                <td>' . $name . '</td>
+            </tr>
+            <tr>
+                <td>Email:</td>
+                <td>' . $email . '</td>
+            </tr>
+            <tr>
+                <td>Address:</td>
+                <td>' . $address . '</td>
+            </tr>
+        </table>
+    ';
+
+    $subject = empty($_POST['subject']) || !isset($_POST['subject']) ? "Spacework website" : $_POST['subject'];
+    
+  $mail = mail( "4au.4au.4au@gmail.com", $subject, $message, $headers );
+
+
+  if($mail){
+    header('HTTP/1.1 200 OK');
+    header('Content-Type: application/json; charset=UTF-8');
+    die(json_encode(array('message' => 'Message send successful', 'code' => 200)));
+  }
+
+?>
